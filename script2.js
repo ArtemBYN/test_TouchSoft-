967c2f8c8e2c8725928cdee592e84645e8c1197f
ArtemB <mil.basalyga@gmail.com>
2021-11-26 14:31:48 +0300

    the second task

---
+++ script2.js
@@ -0,0 +1,9 @@
+function sayHello(name){
+               if (name == 'Mark'){
+        return `Hi, ${name}!`;
+    } else{
+        return `Hello, ${name}!`;
+    }
+}   
+console.log(sayHello("Mark"));
+console.log(sayHello("Viktor"));
