3a2131b683cfa0d3d76de73d520f8c322315ee7e
ArtemBYN <59662879+ArtemBYN@users.noreply.github.com>
2021-11-26 11:01:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,3 @@ function capitalize(str) {
     };
     return s;
 }
-
-
-
-
-
-// let str = "каждый охотник желает знать";  
-
-// function capitalize(str) {
-
-//  return str.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()})
-
-// }
-
-// console.log(capitalize(str));
